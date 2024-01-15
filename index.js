581e0f599b1f16685ecfd6d0431aee0779794610
Christof Röhl <userpattern@gmail.com>
2024-01-15 10:59:03 +0100

    adding toogle functionality wip

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector("[data-js=bookmark-button]");
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+  console.log("Bookmarked!");
+});
+
+const secondBookmarkButton = document.querySelector(
+  "[data-js=bookmark-button-2]"
+);
+secondBookmarkButton.addEventListener("click", () => {
+  secondBookmarkButton.classList.toggle("bookmark--active");
+  console.log("Bookmarked 2nd card!");
+});
+
+const showAnswerButton = document.querySelector("[data-js=show-answer-button]");
+const firstCardAnswer = document.querySelector("[data-js=card-one-answer]");
+
+showAnswerButton.addEventListener("click", () => {
+  firstCardAnswer.classList.toggle("card__answer--active");
+  showAnswerButton.textContent = "Hide answer";
+  console.log("Toggled");
+});
